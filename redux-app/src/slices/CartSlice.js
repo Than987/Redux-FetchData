8ab979c961721d4fa6110e6e0c74a6128a69248c
Than singh <rishidogra987@gmail.com>
2023-08-18 12:13:42 +0530

    used createAsyncThunk middleware to fetch data

--- redux-app/src/slices/CartSlice.js
+++ redux-app/src/slices/CartSlice.js
@@ -1,11 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-
+export const FetchData = createAsyncThunk("FetchData", async()=>{
+  let data = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+  let result = await data.json();
+  return result;
+})
 
 const CartSlice = createSlice({
     name : 'cart',
     initialState:{
-      
+     
     },
    
 })
