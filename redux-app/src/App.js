4c707f1483a1dca97777781d0175261d86425746
Than singh <rishidogra987@gmail.com>
2023-08-18 12:11:53 +0530

    integrated files and folders for basic react-redux setup

---
+++ redux-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h2>FETCHING DATA FROM API USING REACT-REDUX</h2>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ redux-app/src/slices/CartSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const CartSlice = createSlice({
+    name : 'cart',
+    initialState:{
+      
+    },
+   
+})
+export default CartSlice.reducer
