f02b25f36a122d188cba06f4b185c0282280ce23
Than singh <rishidogra987@gmail.com>
2023-08-18 12:16:38 +0530

    displayed the fetched data over screen on button click

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { FetchData } from './slices/CartSlice';
 
 function App() {
-  
+  const state = useSelector((state) => state)
+  console.log("STATE", state);
+  const dispatch = useDispatch();
+
+  if (state.cart.isLoading) {
+    return (
+      <div>
+        <h1 style={{ marginLeft: '30%', color: 'green', fontFamily: 'cursive' }}>Fetching Data From Database...</h1>
+      </div>
+    )
+  }
 
   return (
     <div className="App">
       <h2>FETCHING DATA FROM API USING REACT-REDUX</h2>
-      
+      <button className='btn btn-success' onClick={() => dispatch(FetchData())}>Fetch Items</button>
+      <div>
+        {state.cart.data && state.cart.data.map((item) => <ul style={{ color: 'green', fontSize: '20px' }} key={item.id}><li>{item.title}</li></ul>)}
+      </div>
     </div>
   );
 }
